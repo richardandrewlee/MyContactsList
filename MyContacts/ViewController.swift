ae3db359df80c5c21adc440db3d83d15627ae3b8
Richard Lee <richardandrewlee@yahoo.com>
2017-08-08 11:51:27 -0400

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Courtney Lee on 8/8/17.
+//  Copyright © 2017 Richard Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
